d23a2f5f02c678495885e6a458aba920824339a9
kievzenit <kir.backender@gmail.com>
2024-04-09 17:38:32 +0300

    token class created

---
+++ src/snakelangc/lexer/token.h
@@ -0,0 +1,44 @@
+#ifndef SNAKELANG_TOKEN_H
+#define SNAKELANG_TOKEN_H
+
+#include <string>
+#include <map>
+#include "token_type.h"
+
+namespace lexer {
+
+    class token {
+    public:
+        token(token_type type,
+              std::string value,
+              std::string file_name,
+              int line,
+              int column_start,
+              int column_end) :
+              type(type),
+                value(std::move(value)),
+                file_name(std::move(file_name)),
+                line(line), column_start(column_start), column_end(column_end) {}
+
+        explicit operator std::string() {
+            return std::format(
+                    "token {{ value = {}, line = {}, column_start = {}, column_end = {} }}",
+                    value,
+                    line,
+                    column_start,
+                    column_end);
+        }
+
+        std::string file_name;
+
+        int line;
+        int column_start;
+        int column_end;
+
+        token_type type;
+        std::string value;
+    };
+
+}
+
+#endif //SNAKELANG_TOKEN_H
