86b72e2552f5b53326cc8073ca032ac0221cfcb0
kievzenit <kir.backender@gmail.com>
2024-04-09 17:39:31 +0300

    testing lexer in main.cpp

--- src/snakelangc/main.cpp
+++ src/snakelangc/main.cpp
@@ -1,3 +1,17 @@
+#include <filesystem>
+#include "lexer/lexer.h"
+
 int main() {
+    std::cout << std::filesystem::current_path() << std::endl;
+    lexer::lexer lexer("../../../tests/sources/basic_tokens.sn");
+    lexer::token token = lexer.get_next_token();
+
+    do {
+        std::cout << (std::string)token << std::endl;
+        token = lexer.get_next_token();
+    } while (token.type != lexer::token_type::eof);
+
+    std::cout << (std::string)token << std::endl;
+
     return 0;
 }
