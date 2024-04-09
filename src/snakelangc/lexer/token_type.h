9854eae3925fd0d53ecf18e0ac98df6c311ebc39
kievzenit <kir.backender@gmail.com>
2024-04-09 17:38:24 +0300

    token_type enum created

---
+++ src/snakelangc/lexer/token_type.h
@@ -0,0 +1,49 @@
+#ifndef SNAKELANG_TOKEN_TYPE_H
+#define SNAKELANG_TOKEN_TYPE_H
+
+namespace lexer {
+
+    enum token_type {
+        eof = 1,
+
+        number,
+        boolean,
+
+        identifier,
+
+        let,
+        func,
+        package,
+
+        l_parenthesis,
+        r_parenthesis,
+        l_bracket,
+        r_bracket,
+        l_curly_brace,
+        r_curly_brace,
+
+        plus,
+        minus,
+        asterisk,
+        slash,
+        colon,
+        semicolon,
+        dot,
+        coma,
+
+        assign,
+        plus_assign,
+        minus_assign,
+        multiply_assign,
+        divide_assign,
+
+        equals,
+        less_than,
+        greater_than,
+        less_or_equal,
+        greater_or_equal
+    };
+
+}
+
+#endif //SNAKELANG_TOKEN_TYPE_H
