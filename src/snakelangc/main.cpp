daf7165da8d70d35c8dff9d696cc9b93df0f1766
kievzenit <kir.backender@gmail.com>
2024-04-11 18:03:47 +0300

    main.cpp update regarding last changes

--- src/snakelangc/main.cpp
+++ src/snakelangc/main.cpp
@@ -1,15 +1,14 @@
-#include <filesystem>
-#include "lexer/lexer.h"
 #include "parser/parser.h"
+#include "emitter/emitter.h"
 
 int main() {
     const auto file_name = "../../../tests/sources/basic_tokens.sn";
+
     parser::parser parser(file_name);
     auto translation_ast = parser.parse();
 
-    for (auto &stmt : translation_ast->stmts) {
-        auto let_stmt = dynamic_cast<parser::ast::let_stmt*>(stmt.get());
-    }
+    emitter::emitter emitter(std::move(translation_ast));
+    auto package_ir = emitter.emit();
 
     return 0;
 }
