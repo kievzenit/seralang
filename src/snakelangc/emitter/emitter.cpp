ec8fdf55fa863514cf938fbf1c5e701ea0b13c48
kievzenit <kir.backender@gmail.com>
2024-04-11 18:02:56 +0300

    emitter created

---
+++ src/snakelangc/emitter/emitter.cpp
@@ -0,0 +1,52 @@
+#include "emitter.h"
+
+std::unique_ptr<emitter::ir::package_ir> emitter::emitter::emit() {
+    return std::make_unique<ir::package_ir>(
+            translation_ast_->package->package_name,
+            find_all_global_variables());
+}
+
+std::vector<std::unique_ptr<emitter::ir::variable_ir>> emitter::emitter::find_all_global_variables() {
+    std::vector<std::unique_ptr<ir::variable_ir>> global_variables;
+
+    for (auto &top_stmt : translation_ast_->stmts) {
+        if (dynamic_cast<parser::ast::let_stmt*>(top_stmt.get()) != nullptr) {
+            auto let_stmt = dynamic_cast<parser::ast::let_stmt*>(top_stmt.get());
+
+            auto expr_ir = emit_for_expr(std::move(let_stmt->expression));
+            auto variable_ir = std::make_unique<ir::variable_ir>(
+                    let_stmt->name, std::move(expr_ir), ir::type::int32());
+            global_variables.push_back(std::move(variable_ir));
+        }
+    }
+
+    return global_variables;
+}
+
+std::unique_ptr<emitter::ir::expr_ir> emitter::emitter::emit_for_expr(std::unique_ptr<parser::ast::expr> expr) {
+    if (dynamic_cast<parser::ast::integer_expr*>(expr.get()) != nullptr) {
+        return std::make_unique<ir::integer_expr_ir>(dynamic_cast<parser::ast::integer_expr*>(expr.get()));
+    }
+
+    if (dynamic_cast<parser::ast::boolean_expr*>(expr.get()) != nullptr) {
+        return std::make_unique<ir::boolean_expr_ir>(dynamic_cast<parser::ast::boolean_expr*>(expr.get()));
+    }
+
+    if (dynamic_cast<parser::ast::binary_expr*>(expr.get()) != nullptr) {
+        auto binary_expr = dynamic_cast<parser::ast::binary_expr*>(expr.get());
+
+        auto left_expr_ir = emit_for_expr(std::move(binary_expr->left));
+        auto right_expr_ir = emit_for_expr(std::move(binary_expr->right));
+
+        if (left_expr_ir->expr_type != right_expr_ir->expr_type) {
+            utils::log_error("Left side of expression and right side are different types, so operation cannot be completed!");
+        }
+
+        return std::make_unique<ir::binary_expr_ir>(
+                std::move(left_expr_ir),
+                std::move(right_expr_ir),
+                left_expr_ir->expr_type);
+    }
+
+    utils::log_error("Unexpected expression expr_type, this should never happen!");
+}
---
+++ src/snakelangc/emitter/emitter.h
@@ -0,0 +1,35 @@
+#ifndef SNAKELANG_EMITTER_H
+#define SNAKELANG_EMITTER_H
+
+#include <memory>
+
+#include "../utils/log_error.h"
+#include "../parser/ast/translation_ast.h"
+#include "../parser/ast/let_stmt.h"
+#include "../parser/ast/binary_expr.h"
+#include "ir/package_ir.h"
+#include "ir/binary_expr_ir.h"
+#include "ir/variable_ir.h"
+#include "ir/integer_expr_ir.h"
+#include "ir/boolean_expr_ir.h"
+
+namespace emitter {
+
+    class emitter {
+    public:
+        emitter(std::unique_ptr<parser::ast::translation_ast> translation_ast) :
+            translation_ast_(std::move(translation_ast)) {}
+
+            std::unique_ptr<ir::package_ir> emit();
+
+    private:
+        std::unique_ptr<parser::ast::translation_ast> translation_ast_;
+
+        std::vector<std::unique_ptr<ir::variable_ir>> find_all_global_variables();
+
+        std::unique_ptr<ir::expr_ir> emit_for_expr(std::unique_ptr<parser::ast::expr> expr);
+    };
+
+}
+
+#endif //SNAKELANG_EMITTER_H
