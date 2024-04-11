3560f81301c8a04ba8234a994d50af8426905217
kievzenit <kir.backender@gmail.com>
2024-04-11 09:34:50 +0300

    let_stmt created

---
+++ src/snakelangc/parser/ast/let_stmt.h
@@ -0,0 +1,20 @@
+#ifndef SNAKELANG_LET_STMT_H
+#define SNAKELANG_LET_STMT_H
+
+#include <string>
+#include "expr.h"
+
+namespace parser::ast {
+
+    class let_stmt : public top_stmt {
+    public:
+        let_stmt(std::string name, std::unique_ptr<expr> expression) :
+            name(std::move(name)), expression(std::move(expression)) {}
+
+        std::string name;
+        std::unique_ptr<expr> expression;
+    };
+
+}
+
+#endif //SNAKELANG_LET_STMT_H
