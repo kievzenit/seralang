bf0dede5306af98c92a2a6fa3ad70fc63566f784
kievzenit <kir.backender@gmail.com>
2024-04-11 09:33:43 +0300

    ast_node created

---
+++ src/snakelangc/parser/ast/ast_node.h
@@ -0,0 +1,13 @@
+#ifndef SNAKELANG_AST_NODE_H
+#define SNAKELANG_AST_NODE_H
+
+namespace parser::ast {
+
+    class ast_node {
+    public:
+        virtual ~ast_node() = default;
+    };
+
+}
+
+#endif //SNAKELANG_AST_NODE_H
