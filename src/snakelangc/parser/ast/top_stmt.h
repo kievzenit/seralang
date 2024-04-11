ec26e3fe1e825d7c90e512bfee63db50797b2346
kievzenit <kir.backender@gmail.com>
2024-04-11 09:34:24 +0300

    top_stmt created

---
+++ src/snakelangc/parser/ast/top_stmt.h
@@ -0,0 +1,15 @@
+#ifndef SNAKELANG_TOP_STMT_H
+#define SNAKELANG_TOP_STMT_H
+
+#include "ast_node.h"
+
+namespace parser::ast {
+
+    class top_stmt : public ast_node {
+    public:
+        virtual ~top_stmt() = default;
+    };
+
+}
+
+#endif //SNAKELANG_TOP_STMT_H
