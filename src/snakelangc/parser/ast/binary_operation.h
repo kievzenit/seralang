b412818216c60898285f378a2d0b970fb709d367
kievzenit <kir.backender@gmail.com>
2024-04-11 09:35:47 +0300

    binary_operation created

---
+++ src/snakelangc/parser/ast/binary_operation.h
@@ -0,0 +1,15 @@
+#ifndef SNAKELANG_BINARY_OPERATION_H
+#define SNAKELANG_BINARY_OPERATION_H
+
+namespace parser::ast {
+
+    enum binary_operation {
+        plus,
+        minus,
+        multiply,
+        divide
+    };
+
+}
+
+#endif //SNAKELANG_BINARY_OPERATION_H
