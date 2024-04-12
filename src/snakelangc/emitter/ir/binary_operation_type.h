6a06d8bc4f312cdcdb22c82c9c5d1aeef6cc073b
kievzenit <kir.backender@gmail.com>
2024-04-12 18:11:35 +0300

    binary_operation-type file created

---
+++ src/snakelangc/emitter/ir/binary_operation_type.h
@@ -0,0 +1,15 @@
+#ifndef SNAKELANG_BINARY_OPERATION_TYPE_H
+#define SNAKELANG_BINARY_OPERATION_TYPE_H
+
+namespace emitter::ir {
+
+    enum binary_operation_type {
+        addition,
+        subtraction,
+        multiplication,
+        division
+    };
+
+}
+
+#endif //SNAKELANG_BINARY_OPERATION_TYPE_H
