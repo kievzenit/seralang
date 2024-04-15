cb69ccdcb1c889e8e790c3e5c167e058dbd8c9b9
kievzenit <kir.backender@gmail.com>
2024-04-15 17:38:36 +0300

    added explicit int type member to integer_expr

--- src/snakelangc/parser/ast/integer_expr.h
+++ src/snakelangc/parser/ast/integer_expr.h
@@ -1,3 +1,5 @@
+#include <utility>
+
 #ifndef SNAKELANG_INTEGER_EXPR_H
 #define SNAKELANG_INTEGER_EXPR_H
 
@@ -5,9 +7,11 @@ namespace parser::ast {
 
     class integer_expr : public expr {
     public:
-        integer_expr(int number) : number(number) {}
+        integer_expr(int number, std::string explicit_int_type = "") :
+            number(number), explicit_int_type(std::move(explicit_int_type)) {}
 
         int number;
+        std::string explicit_int_type;
     };
 
 }
