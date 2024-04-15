403f50c68c89291fe487ec3756a056bb48fce3b1
kievzenit <kir.backender@gmail.com>
2024-04-15 19:27:08 +0300

    return_stmt created

---
+++ src/snakelangc/parser/ast/return_stmt.h
@@ -0,0 +1,19 @@
+#ifndef SNAKELANG_RETURN_STMT_H
+#define SNAKELANG_RETURN_STMT_H
+
+#include <memory>
+#include "stmt.h"
+#include "expr.h"
+
+namespace parser::ast {
+
+    class return_stmt : public stmt {
+    public:
+        return_stmt(std::unique_ptr<expr> return_expr) : return_expr(std::move(return_expr)) {}
+
+        std::unique_ptr<expr> return_expr;
+    };
+
+}
+
+#endif //SNAKELANG_RETURN_STMT_H
