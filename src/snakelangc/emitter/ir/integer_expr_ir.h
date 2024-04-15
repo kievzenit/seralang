36ec4e311345349b1214d9fb5735510f07321ec4
kievzenit <kir.backender@gmail.com>
2024-04-15 17:38:59 +0300

    added new constructor to integer_expr_ir to have explicit type

--- src/snakelangc/emitter/ir/integer_expr_ir.h
+++ src/snakelangc/emitter/ir/integer_expr_ir.h
@@ -5,12 +5,17 @@
 #include "type.h"
 #include "expr_ir.h"
 
+#include <utility>
+
 namespace emitter::ir {
 
     class integer_expr_ir : public expr_ir {
     public:
         integer_expr_ir(parser::ast::integer_expr *integer_expr) :
-                expr_ir(type::int32(), true), number(integer_expr->number) {}
+            expr_ir(type::int32(), true), number(integer_expr->number) {}
+
+        integer_expr_ir(parser::ast::integer_expr *integer_expr, type integer_type) :
+            expr_ir(std::move(integer_type), true), number(integer_expr->number) {}
 
         int number;
     };
