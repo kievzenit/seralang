275fcbc9365eff79aab6bfe37b299403cb400aac
kievzenit <kir.backender@gmail.com>
2024-04-17 14:00:32 +0300

    variable_ir is now stmt_ir

--- src/snakelangc/emitter/ir/variable_ir.h
+++ src/snakelangc/emitter/ir/variable_ir.h
@@ -5,10 +5,11 @@
 #include <utility>
 #include "type.h"
 #include "expr_ir.h"
+#include "stmt_ir.h"
 
 namespace emitter::ir {
 
-    class variable_ir {
+    class variable_ir : public stmt_ir {
     public:
         variable_ir(std::string name, std::unique_ptr<expr_ir> expr, type type) :
                 name(std::move(name)), expr(std::move(expr)), variable_type(std::move(type)) {}
