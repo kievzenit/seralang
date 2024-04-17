957435fb5131628052d2256e529e3cd0bb8457ce
kievzenit <kir.backender@gmail.com>
2024-04-17 14:00:18 +0300

    stmt_ir created

---
+++ src/snakelangc/emitter/ir/stmt_ir.h
@@ -0,0 +1,13 @@
+#ifndef SNAKELANG_STMT_IR_H
+#define SNAKELANG_STMT_IR_H
+
+namespace emitter::ir {
+
+    class stmt_ir {
+    public:
+        virtual ~stmt_ir() = default;
+    };
+
+}
+
+#endif //SNAKELANG_STMT_IR_H
