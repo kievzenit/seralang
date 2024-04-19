8cd7f9d3ae0f7ae9fd3b7dfe23c58d68135329df
kievzenit <kir.backender@gmail.com>
2024-04-19 16:59:42 +0300

    added sould be upcasted to binary_expr_ir

--- src/snakelangc/emitter/ir/binary_expr_ir.h
+++ src/snakelangc/emitter/ir/binary_expr_ir.h
@@ -18,7 +18,21 @@ namespace emitter::ir {
             operation_type(operation_type),
             expr_ir(std::move(type), false) {}
 
+        binary_expr_ir(
+                std::unique_ptr<expr_ir> left,
+                std::unique_ptr<expr_ir> right,
+                binary_operation_type operation_type,
+                bool left_should_be_upcasted,
+                bool right_should_be_upcasted,
+                type type) :
+                left(std::move(left)), right(std::move(right)),
+                left_should_be_upcasted(left_should_be_upcasted),
+                right_should_be_upcasted(right_should_be_upcasted),
+                operation_type(operation_type),
+                expr_ir(std::move(type), false) {}
+
         std::unique_ptr<expr_ir> left, right;
+        bool left_should_be_upcasted, right_should_be_upcasted;
         binary_operation_type operation_type;
     };
 
