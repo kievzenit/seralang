48d5f7a320ac18afa1b1b428f70d44ffa98d867a
kievzenit <kir.backender@gmail.com>
2024-04-24 16:08:01 +0300

    added pointer to parent scope in scope_stmt_ir

--- src/snakelangc/emitter/ir/scope_stmt_ir.h
+++ src/snakelangc/emitter/ir/scope_stmt_ir.h
@@ -9,9 +9,35 @@ namespace emitter::ir {
 
     class scope_stmt_ir : public stmt_ir {
     public:
-        scope_stmt_ir(std::vector<std::unique_ptr<stmt_ir>> inner_stmts) : inner_stmts(std::move(inner_stmts)) {}
+        explicit scope_stmt_ir(scope_stmt_ir* parent_scope) : parent_scope(parent_scope) {}
 
         std::vector<std::unique_ptr<stmt_ir>> inner_stmts;
+        scope_stmt_ir *parent_scope;
+        std::map<std::string, type *> variables;
+
+        type* get_type_for_variable(const std::string& name) {
+            if (variables.contains(name)) {
+                return variables[name];
+            }
+
+            if (parent_scope == nullptr) {
+                return nullptr;
+            }
+
+            return parent_scope->get_type_for_variable(name);
+        }
+
+        [[nodiscard]] bool is_var_exists(const std::string& name) const {
+            if (variables.contains(name)) {
+                return true;
+            }
+
+            if (parent_scope == nullptr) {
+                return false;
+            }
+
+            return parent_scope->is_var_exists(name);
+        }
     };
 
 }
