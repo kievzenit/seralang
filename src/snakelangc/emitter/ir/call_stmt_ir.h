2f8216f802f603484d513738f126f724c4da6ce7
kievzenit <kir.backender@gmail.com>
2024-04-24 16:07:22 +0300

    call_stmt_ir created

---
+++ src/snakelangc/emitter/ir/call_stmt_ir.h
@@ -0,0 +1,19 @@
+#ifndef SNAKELANG_CALL_STMT_IR_H
+#define SNAKELANG_CALL_STMT_IR_H
+
+#include <memory>
+#include "stmt_ir.h"
+#include "call_expr_ir.h"
+
+namespace emitter::ir {
+
+    class call_stmt_ir : public stmt_ir {
+    public:
+        call_stmt_ir(std::unique_ptr<call_expr_ir> call_expr_ir) : call_expr(std::move(call_expr_ir)) {}
+
+        std::unique_ptr<call_expr_ir> call_expr;
+    };
+
+}
+
+#endif //SNAKELANG_CALL_STMT_IR_H
