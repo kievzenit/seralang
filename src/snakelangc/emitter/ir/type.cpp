523a6111f244064f22381f03894e7ed141972ff3
kievzenit <kir.backender@gmail.com>
2024-04-24 16:05:31 +0300

    updated type

---
+++ src/snakelangc/emitter/ir/type.cpp
@@ -0,0 +1,122 @@
+#include "type.h"
+
+bool emitter::ir::type::can_be_explicitly_casted_to(type* other) const {
+    if (this == int1()) {
+        return other == int8()
+               || other == uint16()
+               || other == int16()
+               || other == uint16()
+               || other == int32()
+               || other == uint32()
+               || other == int64()
+               || other == uint64();
+    }
+
+    if (this == int8()) {
+        return other == int16()
+               || other == int32()
+               || other == int64();
+    }
+
+    if (this == int16()) {
+        return other == int32()
+               || other == int64();
+    }
+
+    if (this == int32()) {
+        return other == int64();
+    }
+
+    return false;
+}
+
+emitter::ir::type* emitter::ir::type::boolean_ = nullptr;
+emitter::ir::type* emitter::ir::type::int1_ = nullptr;
+emitter::ir::type* emitter::ir::type::int8_ = nullptr;
+emitter::ir::type* emitter::ir::type::int16_ = nullptr;
+emitter::ir::type* emitter::ir::type::int32_ = nullptr;
+emitter::ir::type* emitter::ir::type::int64_ = nullptr;
+emitter::ir::type* emitter::ir::type::uint8_ = nullptr;
+emitter::ir::type* emitter::ir::type::uint16_ = nullptr;
+emitter::ir::type* emitter::ir::type::uint32_ = nullptr;
+emitter::ir::type* emitter::ir::type::uint64_ = nullptr;
+
+emitter::ir::type* emitter::ir::type::boolean() {
+    if (!emitter::ir::type::boolean_) {
+        emitter::ir::type::boolean_ = new type("bool", 1, true);
+    }
+
+    return emitter::ir::type::boolean_;
+}
+
+emitter::ir::type* emitter::ir::type::int1() {
+    if (!int1_) {
+        int1_ = new type("int1", 1, true);
+    }
+
+    return int1_;
+}
+
+emitter::ir::type* emitter::ir::type::int8() {
+    if (!int8_) {
+        int8_ = new type("int8", 1, true);
+    }
+
+    return int8_;
+}
+
+emitter::ir::type* emitter::ir::type::int16() {
+    if (!int16_) {
+        int16_ = new type("int16", 2, true);
+    }
+
+    return int16_;
+}
+
+emitter::ir::type* emitter::ir::type::int32() {
+    if (!int32_) {
+        int32_ = new type("int32", 4, true);
+    }
+
+    return int32_;
+}
+
+emitter::ir::type* emitter::ir::type::int64() {
+    if (!int64_) {
+        int64_ = new type("int64", 8, true);
+    }
+
+    return int64_;
+}
+
+emitter::ir::type* emitter::ir::type::uint8() {
+    if (!uint8_) {
+        uint8_ = new type("uint8", 1, true);
+    }
+
+    return uint8_;
+}
+
+emitter::ir::type* emitter::ir::type::uint16() {
+    if (!uint16_) {
+        uint16_ = new type("uint16", 2, true);
+    }
+
+    return uint16_;
+}
+
+emitter::ir::type* emitter::ir::type::uint32() {
+    if (!uint32_) {
+        uint32_ = new type("uint32", 4, true);
+    }
+
+    return uint32_;
+}
+
+emitter::ir::type* emitter::ir::type::uint64() {
+    if (!uint64_) {
+        uint64_ = new type("uint64", 8, true);
+    }
+
+    return uint64_;
+}
--- src/snakelangc/emitter/ir/type.h
+++ src/snakelangc/emitter/ir/type.h
@@ -7,60 +7,37 @@ namespace emitter::ir {
 
     class type {
     public:
-        type(std::string name, int size, bool is_basic = false) : name(std::move(name)), size(size), is_basic(is_basic) {}
+        type(std::string name, int size, bool is_basic = false) :
+            name(std::move(name)), size(size), is_basic(is_basic) {}
 
         bool is_basic;
         std::string name;
         int size;
 
-        [[nodiscard]] bool can_be_explicitly_casted_to(const type& other) const {
-            if (*this == int1()) {
-                return other == int8()
-                    || other == uint16()
-                    || other == int16()
-                    || other == uint16()
-                    || other == int32()
-                    || other == uint32()
-                    || other == int64()
-                    || other == uint64();
-            }
+        [[nodiscard]] bool can_be_explicitly_casted_to(type* other) const;
 
-            if (*this == int8()) {
-                return other == int16()
-                    || other == int32()
-                    || other == int64();
-            }
+        static type* boolean();
+        static type* int1();
+        static type* int8();
+        static type* int16();
+        static type* int32();
+        static type* int64();
+        static type* uint8();
+        static type* uint16();
+        static type* uint32();
+        static type* uint64();
 
-            if (*this == int16()) {
-                return other == int32()
-                    || other == int64();
-            }
-
-            if (*this == int32()) {
-                return other == int64();
-            }
-
-            return false;
-        }
-
-        bool operator==(const type &other) const {
-            return is_basic == other.is_basic && size == other.size && name == other.name;
-        }
-
-        bool operator!=(const type &other) const {
-            return is_basic != other.is_basic || size != other.size || name != other.name;
-        }
-
-        static type boolean() { return {"bool", 1, true}; }
-        static type int1() { return {"int1", 1, true}; }
-        static type int8() { return {"int8", 1, true}; }
-        static type int16() { return {"int16", 2, true}; }
-        static type int32() { return {"int32", 4, true}; }
-        static type int64() { return {"int64", 8, true}; }
-        static type uint8() { return {"uint8", 1, true}; }
-        static type uint16() { return {"uint16", 2, true}; }
-        static type uint32() { return {"uint32", 4, true}; }
-        static type uint64() { return {"uint64", 8, true}; }
+    private:
+        static type* boolean_;
+        static type* int1_;
+        static type* int8_;
+        static type* int16_;
+        static type* int32_;
+        static type* int64_;
+        static type* uint8_;
+        static type* uint16_;
+        static type* uint32_;
+        static type* uint64_;
     };
 
 }
