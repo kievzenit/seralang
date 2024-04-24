7b91396fb4a41ae10f411ddcaeccc9cfcb8f56ce
kievzenit <kir.backender@gmail.com>
2024-04-24 16:06:53 +0300

    integer_expr_ir now uses pointer to type

--- src/snakelangc/emitter/ir/integer_expr_ir.h
+++ src/snakelangc/emitter/ir/integer_expr_ir.h
@@ -11,11 +11,11 @@ namespace emitter::ir {
 
     class integer_expr_ir : public expr_ir {
     public:
-        integer_expr_ir(parser::ast::integer_expr *integer_expr) :
+        explicit integer_expr_ir(parser::ast::integer_expr *integer_expr) :
             expr_ir(type::int32(), true), number(integer_expr->number) {}
 
-        integer_expr_ir(parser::ast::integer_expr *integer_expr, type integer_type) :
-            expr_ir(std::move(integer_type), true), number(integer_expr->number) {}
+        integer_expr_ir(parser::ast::integer_expr *integer_expr, type* integer_type) :
+            expr_ir(integer_type, true), number(integer_expr->number) {}
 
         int number;
     };
