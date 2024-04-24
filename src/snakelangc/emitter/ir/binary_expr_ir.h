f43f25cb223b3edfdd3a59aa6afb47dea97dee9b
kievzenit <kir.backender@gmail.com>
2024-04-24 16:07:40 +0300

    binary_expr_ir now uses pointer to type

--- src/snakelangc/emitter/ir/binary_expr_ir.h
+++ src/snakelangc/emitter/ir/binary_expr_ir.h
@@ -13,10 +13,10 @@ namespace emitter::ir {
                 std::unique_ptr<expr_ir> left,
                 std::unique_ptr<expr_ir> right,
                 binary_operation_type operation_type,
-                type type) :
+                type* type) :
             left(std::move(left)), right(std::move(right)),
             operation_type(operation_type),
-            expr_ir(std::move(type), false) {}
+            expr_ir(type, false) {}
 
         binary_expr_ir(
                 std::unique_ptr<expr_ir> left,
@@ -24,12 +24,12 @@ namespace emitter::ir {
                 binary_operation_type operation_type,
                 bool left_should_be_upcasted,
                 bool right_should_be_upcasted,
-                type type) :
+                type* type) :
                 left(std::move(left)), right(std::move(right)),
                 left_should_be_upcasted(left_should_be_upcasted),
                 right_should_be_upcasted(right_should_be_upcasted),
                 operation_type(operation_type),
-                expr_ir(std::move(type), false) {}
+                expr_ir(type, false) {}
 
         std::unique_ptr<expr_ir> left, right;
         bool left_should_be_upcasted, right_should_be_upcasted;
