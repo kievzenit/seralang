c1072ce83cfa30d710c4f8ac2fbee02da4e2517c
kievzenit <kir.backender@gmail.com>
2024-04-25 19:32:34 +0300

    downcast_expr_ir created

---
+++ src/snakelangc/emitter/ir/downcast_expr_ir.h
@@ -0,0 +1,16 @@
+#ifndef SNAKELANG_DOWNCAST_EXPR_IR_H
+#define SNAKELANG_DOWNCAST_EXPR_IR_H
+
+#include "cast_expr_ir.h"
+
+namespace emitter::ir {
+
+    class downcast_expr_ir : public cast_expr_ir {
+    public:
+        downcast_expr_ir(type* downcast_to_type, std::unique_ptr<expr_ir> inner_expr) :
+            cast_expr_ir(downcast_to_type, std::move(inner_expr)) {}
+    };
+
+}
+
+#endif //SNAKELANG_DOWNCAST_EXPR_IR_H
