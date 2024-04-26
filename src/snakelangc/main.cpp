6077cc97f4962f1b069bbfefb91c0843aefefd7b
kievzenit <kir.backender@gmail.com>
2024-04-26 13:45:15 +0300

    main.cpp updated to use compiler

--- src/snakelangc/main.cpp
+++ src/snakelangc/main.cpp
@@ -1,18 +1,11 @@
-#include "parser/parser.h"
-#include "emitter/emitter.h"
-#include "translator/translator.h"
+#include "compiler/compiler.h"
 
 int main() {
-    const auto file_name = "../../../tests/sources/main.sn";
-
-    parser::parser parser(file_name);
-    auto translation_ast = parser.parse();
-
-    emitter::emitter emitter(std::move(translation_ast));
-    auto package_ir = emitter.emit();
-
-    translator::translator translator(std::move(package_ir));
-    translator.translate();
+    compiler::compiler compiler({
+        std::filesystem::current_path(),
+        compiler::emit_type::llvm_ir
+    });
+    compiler.compile();
 
     return 0;
 }
