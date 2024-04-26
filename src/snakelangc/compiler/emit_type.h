74c7ff4404615d2f42ee34ec37a6233c10cf5f17
kievzenit <kir.backender@gmail.com>
2024-04-26 13:44:18 +0300

    emit_type enum created

---
+++ src/snakelangc/compiler/emit_type.h
@@ -0,0 +1,14 @@
+#ifndef SNAKELANG_EMIT_TYPE_H
+#define SNAKELANG_EMIT_TYPE_H
+
+namespace compiler {
+
+    enum emit_type {
+        object_file,
+        assembler,
+        llvm_ir
+    };
+
+}
+
+#endif //SNAKELANG_EMIT_TYPE_H
