bf893aef780ecf0441f20b7ef6ac8df450a58365
kievzenit <kir.backender@gmail.com>
2024-04-30 15:00:31 +0300

    variable_ir added new property

--- src/snakelangc/emitter/ir/variable_ir.h
+++ src/snakelangc/emitter/ir/variable_ir.h
@@ -11,13 +11,18 @@ namespace emitter::ir {
 
     class variable_ir : public stmt_ir {
     public:
-        variable_ir(std::string name, std::unique_ptr<expr_ir> expr, type* type, bool is_static) :
-                name(std::move(name)), expr(std::move(expr)), variable_type(type), is_static(is_static) {}
+        variable_ir(std::string name, std::unique_ptr<expr_ir> expr, type* type, bool is_static, bool is_private) :
+                name(std::move(name)),
+                expr(std::move(expr)),
+                variable_type(type),
+                is_static(is_static),
+                is_private(is_private) {}
 
         type* variable_type;
         std::string name;
         std::unique_ptr<expr_ir> expr;
         bool is_static;
+        bool is_private;
     };
 
 }
