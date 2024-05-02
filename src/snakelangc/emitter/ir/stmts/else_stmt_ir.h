9cfaae3ce5dba425b5ff184bca31071bab99c282
kievzenit <kir.backender@gmail.com>
2024-05-02 15:06:38 +0300

    else_stmt_ir created

---
+++ src/snakelangc/emitter/ir/stmts/else_stmt_ir.h
@@ -0,0 +1,19 @@
+#ifndef SNAKELANG_ELSE_STMT_IR_H
+#define SNAKELANG_ELSE_STMT_IR_H
+
+#include <memory>
+#include "stmt_ir.h"
+#include "scope_stmt_ir.h"
+
+namespace emitter::ir {
+
+    class else_stmt_ir : public stmt_ir {
+    public:
+        explicit else_stmt_ir(std::unique_ptr<scope_stmt_ir> scope) : scope(std::move(scope)) {}
+
+        std::unique_ptr<scope_stmt_ir> scope;
+    };
+
+}
+
+#endif //SNAKELANG_ELSE_STMT_IR_H
