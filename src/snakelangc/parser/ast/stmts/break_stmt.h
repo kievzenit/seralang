e50ce4c0e5a68409e8e427656406edc7008ad36c
kievzenit <kir.backender@gmail.com>
2024-05-03 14:46:17 +0300

    break_stmt created

---
+++ src/snakelangc/parser/ast/stmts/break_stmt.h
@@ -0,0 +1,12 @@
+#ifndef SNAKELANG_BREAK_STMT_H
+#define SNAKELANG_BREAK_STMT_H
+
+#include "stmt.h"
+
+namespace parser::ast {
+
+    class break_stmt : public stmt {};
+
+}
+
+#endif //SNAKELANG_BREAK_STMT_H
