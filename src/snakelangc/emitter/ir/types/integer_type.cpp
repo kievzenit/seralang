221c6c36882461998191d5f74bec5a954a3df928
kievzenit <kir.backender@gmail.com>
2024-05-03 17:54:08 +0300

    integer_type created

---
+++ src/snakelangc/emitter/ir/types/integer_type.cpp
@@ -0,0 +1,113 @@
+#include "integer_type.h"
+
+bool emitter::ir::integer_type::can_be_implicitly_casted_to(type* other) const {
+    if (this == int1()) {
+        return other == int8()
+               || other == uint16()
+               || other == int16()
+               || other == uint16()
+               || other == int32()
+               || other == uint32()
+               || other == int64()
+               || other == uint64();
+    }
+
+    if (this == int8()) {
+        return other == int16()
+               || other == int32()
+               || other == int64();
+    }
+
+    if (this == int16()) {
+        return other == int32()
+               || other == int64();
+    }
+
+    if (this == int32()) {
+        return other == int64();
+    }
+
+    return false;
+}
+
+emitter::ir::integer_type* emitter::ir::integer_type::int1_ = nullptr;
+emitter::ir::integer_type* emitter::ir::integer_type::int8_ = nullptr;
+emitter::ir::integer_type* emitter::ir::integer_type::int16_ = nullptr;
+emitter::ir::integer_type* emitter::ir::integer_type::int32_ = nullptr;
+emitter::ir::integer_type* emitter::ir::integer_type::int64_ = nullptr;
+emitter::ir::integer_type* emitter::ir::integer_type::uint8_ = nullptr;
+emitter::ir::integer_type* emitter::ir::integer_type::uint16_ = nullptr;
+emitter::ir::integer_type* emitter::ir::integer_type::uint32_ = nullptr;
+emitter::ir::integer_type* emitter::ir::integer_type::uint64_ = nullptr;
+
+emitter::ir::integer_type* emitter::ir::integer_type::int1() {
+    if (!int1_) {
+        int1_ = new integer_type("int1", 1, false);
+    }
+
+    return int1_;
+}
+
+emitter::ir::integer_type* emitter::ir::integer_type::int8() {
+    if (!int8_) {
+        int8_ = new integer_type("int8", 1, false);
+    }
+
+    return int8_;
+}
+
+emitter::ir::integer_type* emitter::ir::integer_type::int16() {
+    if (!int16_) {
+        int16_ = new integer_type("int16", 2, false);
+    }
+
+    return int16_;
+}
+
+emitter::ir::integer_type* emitter::ir::integer_type::int32() {
+    if (!int32_) {
+        int32_ = new integer_type("int32", 4, false);
+    }
+
+    return int32_;
+}
+
+emitter::ir::integer_type* emitter::ir::integer_type::int64() {
+    if (!int64_) {
+        int64_ = new integer_type("int64", 8, false);
+    }
+
+    return int64_;
+}
+
+emitter::ir::integer_type* emitter::ir::integer_type::uint8() {
+    if (!uint8_) {
+        uint8_ = new integer_type("uint8", 1, true);
+    }
+
+    return uint8_;
+}
+
+emitter::ir::integer_type* emitter::ir::integer_type::uint16() {
+    if (!uint16_) {
+        uint16_ = new integer_type("uint16", 2, true);
+    }
+
+    return uint16_;
+}
+
+emitter::ir::integer_type* emitter::ir::integer_type::uint32() {
+    if (!uint32_) {
+        uint32_ = new integer_type("uint32", 4, true);
+    }
+
+    return uint32_;
+}
+
+emitter::ir::integer_type* emitter::ir::integer_type::uint64() {
+    if (!uint64_) {
+        uint64_ = new integer_type("uint64", 8, true);
+    }
+
+    return uint64_;
+}
--- src/snakelangc/emitter/ir/types/type.h
+++ src/snakelangc/emitter/ir/types/type.h
@@ -8,40 +8,18 @@ namespace emitter::ir {
     class type {
     public:
         type(std::string name, int size, bool is_basic = false) :
-            name(std::move(name)), size(size), is_basic(is_basic), is_unsigned(false) {}
+            name(std::move(name)), size(size), is_basic(is_basic) {}
 
         bool is_basic;
         std::string name;
         int size;
-        bool is_unsigned;
 
-        [[nodiscard]] bool can_be_implicitly_casted_to(type* other) const;
+        [[nodiscard]] virtual bool can_be_implicitly_casted_to(type* other) const;
 
         static type* boolean();
-        static type* int1();
-        static type* int8();
-        static type* int16();
-        static type* int32();
-        static type* int64();
-        static type* uint8();
-        static type* uint16();
-        static type* uint32();
-        static type* uint64();
 
     private:
-        type(std::string name, int size, bool is_basic, bool is_unsigned) :
-            name(std::move(name)), size(size), is_basic(is_basic), is_unsigned(is_unsigned) {}
-
         static type* boolean_;
-        static type* int1_;
-        static type* int8_;
-        static type* int16_;
-        static type* int32_;
-        static type* int64_;
-        static type* uint8_;
-        static type* uint16_;
-        static type* uint32_;
-        static type* uint64_;
     };
 
 }
