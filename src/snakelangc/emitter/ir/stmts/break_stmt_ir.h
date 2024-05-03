9e6b9f462178353c9fd0bc5861e6c2f43599c5aa
kievzenit <kir.backender@gmail.com>
2024-05-03 14:49:04 +0300

    break_stmt_ir created

---
+++ src/snakelangc/emitter/ir/stmts/break_stmt_ir.h
@@ -0,0 +1,12 @@
+#ifndef SNAKELANG_BREAK_STMT_IR_H
+#define SNAKELANG_BREAK_STMT_IR_H
+
+#include "stmt_ir.h"
+
+namespace emitter::ir {
+
+    class break_stmt_ir : public stmt_ir {};
+
+}
+
+#endif //SNAKELANG_BREAK_STMT_IR_H
