43598012488e1c33d47de33883220e9b1bb2a918
kievzenit <kir.backender@gmail.com>
2024-05-03 16:18:54 +0300

    continue_stmt_ir created

---
+++ src/snakelangc/emitter/ir/stmts/continue_stmt_ir.h
@@ -0,0 +1,12 @@
+#ifndef SNAKELANG_CONTINUE_STMT_IR_H
+#define SNAKELANG_CONTINUE_STMT_IR_H
+
+#include "stmt_ir.h"
+
+namespace emitter::ir {
+
+    class continue_stmt_ir : public stmt_ir {};
+
+}
+
+#endif //SNAKELANG_CONTINUE_STMT_IR_H
