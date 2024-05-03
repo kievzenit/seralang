59fd2f170a7d60e24053a54506e5894b02d020ce
kievzenit <kir.backender@gmail.com>
2024-05-03 14:46:35 +0300

    breakall_stmt created

---
+++ src/snakelangc/parser/ast/stmts/breakall_stmt.h
@@ -0,0 +1,12 @@
+#ifndef SNAKELANG_BREAKALL_STMT_H
+#define SNAKELANG_BREAKALL_STMT_H
+
+#include "stmt.h"
+
+namespace parser::ast {
+
+    class breakall_stmt : public stmt {};
+
+}
+
+#endif //SNAKELANG_BREAKALL_STMT_H
