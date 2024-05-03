f2b1fefb54d66379a0cb8db1413cc111c3279e58
kievzenit <kir.backender@gmail.com>
2024-05-03 16:15:22 +0300

    continue_stmt created

---
+++ src/snakelangc/parser/ast/stmts/continue_stmt.h
@@ -0,0 +1,12 @@
+#ifndef SNAKELANG_CONTINUE_STMT_H
+#define SNAKELANG_CONTINUE_STMT_H
+
+#include "stmt.h"
+
+namespace parser::ast {
+
+    class continue_stmt : public stmt {};
+
+}
+
+#endif //SNAKELANG_CONTINUE_STMT_H
