d18578c49896cc4101d3338f82e5ab59947dc680
kievzenit <kir.backender@gmail.com>
2024-05-08 17:27:03 +0300

    compound_stmt created

---
+++ src/snakelangc/parser/ast/stmts/compound_stmt.h
@@ -0,0 +1,12 @@
+#ifndef SNAKELANG_COMPOUND_STMT_H
+#define SNAKELANG_COMPOUND_STMT_H
+
+#include "stmt.h"
+
+namespace parser::ast {
+
+    class compound_stmt : public stmt {};
+
+}
+
+#endif //SNAKELANG_COMPOUND_STMT_H
