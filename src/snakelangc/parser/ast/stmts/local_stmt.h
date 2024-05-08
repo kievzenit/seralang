03389b67892fbbefacbb0d23fa6390db69390fd9
kievzenit <kir.backender@gmail.com>
2024-05-08 17:27:23 +0300

    local_stmt created

---
+++ src/snakelangc/parser/ast/stmts/local_stmt.h
@@ -0,0 +1,13 @@
+#ifndef SNAKELANG_LOCAL_STATEMENT_H
+#define SNAKELANG_LOCAL_STATEMENT_H
+
+#include "stmt.h"
+
+namespace parser::ast {
+
+    class local_stmt : public stmt {};
+
+}
+
+
+#endif //SNAKELANG_LOCAL_STATEMENT_H
