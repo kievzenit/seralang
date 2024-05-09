2cace163d39e35f1ca5c727f97bb8fe78ec1f5ba
kievzenit <kir.backender@gmail.com>
2024-05-09 14:18:19 +0300

    updated main to support new compile_options interface

--- src/serac/main.cpp
+++ src/serac/main.cpp
@@ -1,10 +1,8 @@
 #include "compiler/compiler.h"
 
 int main() {
-    compiler::compiler compiler({
-        std::filesystem::current_path(),
-        compiler::emit_type::llvm_ir
-    });
+    compiler::compile_options options;
+    compiler::compiler compiler(options);
     compiler.compile();
 
     return 0;
