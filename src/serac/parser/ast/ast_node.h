eb3085309ba9ed0dde94cc6d430ca61172816f08
kievzenit <kir.backender@gmail.com>
2024-05-17 16:44:14 +0300

    added metadata to ast_node

--- src/serac/parser/ast/ast_node.h
+++ src/serac/parser/ast/ast_node.h
@@ -1,11 +1,17 @@
 #ifndef SERALANG_AST_NODE_H
 #define SERALANG_AST_NODE_H
 
+#include <utility>
+
+#include "common/file_metadata.h"
+
 namespace parser::ast {
 
     class ast_node {
     public:
         virtual ~ast_node() = default;
+
+        file_metadata metadata;
     };
 
 }
