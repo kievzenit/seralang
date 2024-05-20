ad4ea0b1f1a28270324a4882f37892786465054b
kievzenit <kir.backender@gmail.com>
2024-05-20 15:09:58 +0300

    renamed scope in func_decl_ir

--- src/serac/emitter/ir/func_decl_ir.h
+++ src/serac/emitter/ir/func_decl_ir.h
@@ -16,16 +16,16 @@ namespace emitter::ir {
                 std::string name,
                 std::vector<func_param_ir> params,
                 type* return_type,
-                std::unique_ptr<scope_stmt_ir> root_scope_stmt) :
-            name(std::move(name)),
-            params(std::move(params)),
-            return_type(return_type),
-            root_scope_stmt(std::move(root_scope_stmt)) {}
+                std::unique_ptr<scope_stmt_ir> scope) :
+                name(std::move(name)),
+                params(std::move(params)),
+                return_type(return_type),
+                scope(std::move(scope)) {}
 
         std::string name;
         std::vector<func_param_ir> params;
         type* return_type;
-        std::unique_ptr<scope_stmt_ir> root_scope_stmt;
+        std::unique_ptr<scope_stmt_ir> scope;
     };
 
 }
